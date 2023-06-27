da1b0d08d870532806f80cb27ccf4a5b3a83b91b
ChriMarc71 <94993644+ChriMarc71@users.noreply.github.com>
2023-06-27 10:01:17 +0200

    Update getBookingsController.js

--- src/controllers/bookings/getBookingsController.js
+++ src/controllers/bookings/getBookingsController.js
@@ -1,16 +1,21 @@
-const { prisma } = require("../../database/connection"); 
-const getBooking = async (req,res) => {
-    let searched = req.body.patientUsername;
-    let bookings;
-    if (typeof searched != "undefined" ){
-         bookings = await prisma.bookings.findUnique({
-            where:{
-                Username:searched
-            }
-        })
-    }else{
-         bookings = await prisma.bookings.findMany({})
-    }
-    res.send(bookings);
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getBooking = async (req: Request, res: Response) => {
+  let searched = req.body.patientUsername;
+  let bookings;
+  if (typeof searched != "undefined") {
+    bookings = await prisma.bookings.findUnique({
+      where: {
+        Username: searched,
+      },
+    });
+  } else {
+    bookings = await prisma.bookings.findMany({});
+  }
+  res.send(bookings);
 };
-module.exports = getBooking;
+
+export default getBooking;
