1c5acc0b007bc55db7ce454a5424055168d6594a
ChriMarc71 <chrimarc71@gmail.com>
2023-06-27 16:09:38 +0200

    makeId in crypto

--- src/utils/makeId.ts
+++ src/utils/makeId.ts
@@ -1,13 +1,5 @@
+import crypto from "crypto";
 function makeId(length: number): string {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
+  return crypto.randomBytes(length).toString();
 }
 export { makeId };
