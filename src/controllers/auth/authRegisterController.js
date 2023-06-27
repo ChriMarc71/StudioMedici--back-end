7f484370d25379de38d62f90b07d751283b5db4e
ChriMarc71 <94993644+ChriMarc71@users.noreply.github.com>
2023-06-27 09:57:36 +0200

    Update authRegisterController.js

--- src/controllers/auth/authRegisterController.js
+++ src/controllers/auth/authRegisterController.js
@@ -1,40 +1,41 @@
-const { prisma } = require("../../database/connection"); 
-const {sendConfirmationEmail} = require("./sendConfirmationEmail.js");
-const { makeid } = require("../../utils/makeId"); 
-const express = require("express");
-const isVaildEmail = require("../../utils/mailValidator"); 
+import { prisma } from "../../database/connection";
+import { sendConfirmationEmail } from "./sendConfirmationEmail";
+import { makeid } from "../../utils/makeId";
+import express from "express";
+import isVaildEmail from "../../utils/mailValidator";
+
 const app2 = express();
-const register = async (req, res) => {
+const register = async (req: express.Request, res: express.Response) => {
   const email = req.body.email;
   if(!isVaildEmail(email)){
     res.send("invalid email")
   }else{
-  const username = req.body.username;
-  const password = req.body.password;
-  await prisma.auth.create({
-    data: {
-      Email: email,
-      Username: username,
-      Password: password,
-      IsEnable:false,
-      Token:makeid(64)
-    },
-  });
-  const randomString = makeid(64);
-  sendConfirmationEmail(email,randomString);
-  res.send("Registration done, check emails to confirm the account");
-  app2.get("/"+randomString, async (req,res)=>{
-    await prisma.auth.update({
-      where:{
-        Email:email
+    const username = req.body.username;
+    const password = req.body.password;
+    await prisma.auth.create({
+      data: {
+        Email: email,
+        Username: username,
+        Password: password,
+        IsEnable: false,
+        Token: makeid(64)
       },
-      data:{
-        IsEnable:true
-      }
-    })
-    res.send("Registration confirmed")
-  });
+    });
+    const randomString: string = makeid(64);
+    sendConfirmationEmail(email, randomString);
+    res.send("Registration done, check emails to confirm the account");
+    app2.get("/"+randomString, async (req, res) => {
+      await prisma.auth.update({
+        where:{
+          Email: email
+        },
+        data:{
+          IsEnable: true
+        }
+      })
+      res.send("Registration confirmed")
+    });
   }
 }
 app2.listen(3001)
-module.exports = { register };
+export { register };
