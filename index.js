c175bd95fb16638e9992f2bc8797fc3f22ab3a19
ChriMarc71 <94993644+ChriMarc71@users.noreply.github.com>
2023-06-27 10:06:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require("express")
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({ extended: false }))
+import express from "express";
+import bodyParser from "body-parser";
+
+const app: express.Application = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(8080);
