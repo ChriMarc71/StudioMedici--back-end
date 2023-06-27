2fe4c9bdd3721f99337f487d18872e504a2b87cf
ChriMarc71 <94993644+ChriMarc71@users.noreply.github.com>
2023-06-27 10:00:43 +0200

    Update createBookingController.js

--- src/controllers/bookings/createBookingController.js
+++ src/controllers/bookings/createBookingController.js
@@ -1,13 +1,18 @@
-const { prisma } = require("../../database/connection");
-const createBooking = async (req,res)=>{
-    await prisma.bookings.create({
-        data:{
-            StartTime:req.body.startTime,
-            EndTime:req.body.endTime,
-            PatientUsername:req.body.patient,
-            PatientNumber:req.body.number,
-            DoctorUsername: req.body.doctorUsername
-        }
-    })
-}
-module.exports = createBooking;
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createBooking = async (req: Request, res: Response) => {
+  await prisma.bookings.create({
+    data: {
+      StartTime: req.body.startTime,
+      EndTime: req.body.endTime,
+      PatientUsername: req.body.patient,
+      PatientNumber: req.body.number,
+      DoctorUsername: req.body.doctorUsername,
+    },
+  });
+};
+
+export default createBooking;
