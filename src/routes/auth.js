53dde02e099a0af77a9fdeeb59ac26a37ce12c46
Simone Rimedio <77270481+SimoneRimedio@users.noreply.github.com>
2023-06-27 10:04:04 +0200

    Delete auth.js

--- src/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const login = require("../controllers/authControllers/login")
-const register = require("../controllers/authControllers/register")
-
-module.exports = router;
